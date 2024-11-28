d3baf48e1c4f985d4ecd73ae4570d2f0fe775b6f
MohammadFahad1 <fahad4bangladesh@gmail.com>
2024-11-28 08:12:51 +0600

    Python Loops and User input Practice [Done]

---
+++ Loops.py
@@ -0,0 +1,36 @@
+# num=1
+""" while num <= 10:
+    print(num)
+    num+=1 """
+
+""" while num < 10:
+    if num==5:
+        num+=1
+        continue
+        # break
+    print(num)
+    num+=1
+     """
+
+""" numbers = [10,20,30,40,50,60]
+sum = 0
+for num in numbers:
+    print(num)
+    if(num > 30):
+        sum+=num
+print("Sum of big values=",sum) """
+
+""" text = "Pagla hawar tore"
+for char in text:
+    print(char) """
+
+# Range parameters 1. Start, 2. Stop, 3. Increment by
+""" for num in range(0,100,10):
+    print(num) """
+
+""" for i in range(5):
+    print(i) """
+
+x = 10
+y = 2
+print(x // y)
---
+++ input.py
@@ -0,0 +1,8 @@
+""" name = input("What is your name:")
+print("Your name is:", name) """
+
+# Input stores the value as string by default
+# Dont forget to convert the value into number (integer) while taking a number input from a user!
+age = int(input("How old are you:"))
+print(type(age))
+print("Biyer boyosh par hoise aj", age - 21, "bochor hoilo! but biye hoilo na!")
