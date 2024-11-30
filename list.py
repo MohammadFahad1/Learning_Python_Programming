efef0036b65d59e06f9ac9f64cd0eda357f9ef8c
MohammadFahad1 <fahad4bangladesh@gmail.com>
2024-11-30 23:55:53 +0600

    Python lists

---
+++ list.py
@@ -0,0 +1,14 @@
+#           0  1  2  3  4  5  6  7  8  9
+numbers = [10,20,30,40,50,60,70,80,90,100]
+#         -10 -9 -8  -7 -6 -5 -4 -3 -2  -1
+print(numbers[3:8]) # Start index to last index
+print(numbers[3:8:2]) # Increment by 2
+print(numbers[8:3:-1]) # Reversed printing
+print(numbers[8:3:-2]) # Reversed decreasing by 2
+print(numbers[3:])
+print(numbers[:8])
+print(numbers[3::-1])
+print(numbers[::-3])
+print(numbers[::-1])
+print(numbers[::])
+print(numbers)
