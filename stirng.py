f2511bf655ae766e51c52ffdfcf0c976fadc546c
MohammadFahad1 <fahad4bangladesh@gmail.com>
2024-12-01 23:14:10 +0600

    String and String built in methods

---
+++ stirng.py
@@ -0,0 +1,21 @@
+name = 'Fahad\'s Munshi' # escape string
+name2 = "Fahad Munshi"
+name3 = """
+    Fahad Munshi
+    Number one
+"""
+
+""" print(name)
+print(name2)
+print(name3) """
+
+# String is a sequence of characters
+# for char in name2:
+    # print(char)
+print(name2[::-1])
+if "Munshi" in name2:
+    print("Exists")
+# Mutable means changeable
+# Immutable means you not change it
+
+print(name.upper())
