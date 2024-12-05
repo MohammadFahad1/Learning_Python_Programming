41f638b207299095cbd74ac93ad25e11a1c732c2
MohammadFahad1 <fahad4bangladesh@gmail.com>
2024-12-05 22:20:27 +0600

    Good Sequence Problem Solving is not done yet

--- C_Good_Sequence.py
+++ C_Good_Sequence.py
@@ -1,20 +1,10 @@
 from collections import Counter
 
-def min_removals_to_good_sequence(N, a):
-    freq = Counter(a)
-    removals = 0
-    
-    for x, count in freq.items():
-        if count > x:
-            removals += count - x
-        else:
-            removals += count
-    
-    return removals
-
-# Input
 N = int(input())
 a = list(map(int, input().split()))
 
-# Output
-print(min_removals_to_good_sequence(N, a))
+freq = Counter()
+
+for i in a:
+    freq[i] = freq[i] + 1
+
