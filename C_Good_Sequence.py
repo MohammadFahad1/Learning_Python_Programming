560661aa9f20765cf3b0bc6e53d3069e950541d8
MohammadFahad1 <fahad4bangladesh@gmail.com>
2024-12-07 20:58:08 +0600

    Good Sequence problem solved

--- C_Good_Sequence.py
+++ C_Good_Sequence.py
@@ -1,10 +1,19 @@
-from collections import Counter
-
 N = int(input())
 a = list(map(int, input().split()))
 
-freq = Counter()
+freq = {key: 0 for key in a}
 
 for i in a:
     freq[i] = freq[i] + 1
 
+removeCount = 0
+
+arr = set(a)
+
+for i in arr:
+    if freq[i] > i:
+        removeCount += freq[i] - i
+    elif freq[i] < i:
+        removeCount = abs(removeCount) + freq[i]
+
+print(removeCount)
